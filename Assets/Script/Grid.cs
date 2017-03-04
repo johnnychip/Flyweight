717ac48c3e75dcccdd064b1939f746aad330f7cd
John Gomez <johngomez1115@gmail.com>
2017-03-04 13:18:03 -0500

    Initial Commit

---
+++ Assets/Script/Grid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+
+	[SerializeField]
+	private int mapSizeX;
+	[SerializeField]
+	private int mapSizeY;
+	[SerializeField]
+	private GameObject tilePrefab;
+
+
+	private Tile[,] tiles;
+
+	void Start()
+	{
+	
+		tiles = new Tile[mapSizeX, mapSizeY]; 
+
+		for (int x = 0; x < mapSizeX; x++) {
+		
+			for (int y = 0; y < mapSizeY; y++) {
+			
+				GameObject tempCube = Instantiate (tilePrefab, new Vector3 (x, y, 0f), Quaternion.identity);
+				tiles [x, y] = tempCube.GetComponent<Tile> ();
+			
+			}
+
+		}
+
+	}
+
+}
---
+++ Assets/Script/Unit.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+	[SerializeField]
+	private int health;
+	[SerializeField]
+	private int attack;
+	[SerializeField]
+	private int defense;
+	[SerializeField]
+	private Team myTeam;
+	[SerializeField]
+	private Vector2 gridPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Attack(Unit otherUnit){
+		if (otherUnit.MyTeam != myTeam) {
+			otherUnit.Damage (attack);
+		}
+	}
+
+	public void Move(){
+	}
+
+	public void Damage(int enemyAttack){
+		health -= defense - enemyAttack;
+		if (health <= 0)
+			gameObject.SetActive (false);
+	}
+
+	public Team MyTeam {
+		get {
+			return myTeam;
+		}
+	}
+
+	public int Health {
+		get {
+			return health;
+		}
+		set {
+			health = value;
+		}
+	}
+}
