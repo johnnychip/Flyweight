e9b130d9389150e1dc6dfb194d34ded0d2470265
John Gomez <johngomez1115@gmail.com>
2017-03-04 17:59:40 -0500

    Little modification to a script

--- Assets/Script/Tile.cs
+++ Assets/Script/Tile.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour {
 
-
+	private Vector2 TilePosition;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+	public void SetTilePosition(int tempX, int tempY)
+	{
+		TilePosition = new Vector2 (tempX, tempY);
+	}
+
+	public Vector2 GetTilePosition()
+	{
+		return TilePosition;
+	}
+		
 	// Update is called once per frame
 	void Update () {
 		
